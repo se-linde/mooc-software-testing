05e6835d349b87267e6628e6a51cf3dca67c2de0
Toph Bei Fong <tophbeifong@mac.lan>
2024-12-18 00:18:42 -0800

    completed the test class for addFive

--- src/main/java/tudelft/gettingstarted/GettingStarted.java
+++ src/main/java/tudelft/gettingstarted/GettingStarted.java
@@ -1,7 +1,13 @@
 package tudelft.gettingstarted;
 
+
+// import sun.jvm.hotspot.utilities.Assert;
+
+import sun.jvm.hotspot.utilities.Assert;
+
 public class GettingStarted {
     public int addFive (int number) {
         return number + 5;
     }
 }
+
--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -3,6 +3,7 @@ package tudelft.gettingstarted;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+
 public class GettingStartedTest {
 
     @Test
@@ -10,6 +11,19 @@ public class GettingStartedTest {
         int result = new GettingStarted().addFive(20);
         Assertions.assertEquals(25,result);
     }
+
+    @Test
+    public void addFiveToZero() {
+        int result = new GettingStarted().addFive(0);
+        Assertions.assertEquals(5, result);
+    }
+
+    @Test
+    public void addFiveToMinus20() {
+        int result = new GettingStarted().addFive(-20);
+        Assertions.assertEquals(-15, result);
+    }
+
 
 //    UNCOMMENT THE CODE BELOW, AND FILL THE GAPS!
 
